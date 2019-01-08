9411f2d9bba23d6bc793c4a903b4cafeaa411be1
Oliver Pöckelhofer <oliver.poeckelhofer@hotmail.com>
2019-01-08 13:20:54 +0100

    Changed to mobile aspect ratio (2:3) and added camera follow

---
+++ Assets/Scripts/PlayerFollowScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollowScript : MonoBehaviour {
+
+    public Transform PlayerTransform;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(PlayerTransform != null)
+        {
+            transform.position = new Vector3(transform.position.x, PlayerTransform.position.y, transform.position.z);
+
+        }
+        else
+        {
+            Debug.Log("Player Transform null!");
+        }
+	}
+}
