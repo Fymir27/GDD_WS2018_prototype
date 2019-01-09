88ee8f7c8130c6430e853133f0b57bcef16e69ac
Oliver Pöckelhofer <oliver.poeckelhofer@hotmail.com>
2019-01-09 18:49:48 +0100

    Added gitignore for cleanness. Added spikes and game over

---
+++ Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Singleton - only add this once per scene!
+/// </summary>
+public class ControllerScript : MonoBehaviour {
+
+    public static ControllerScript Instance;
+
+    public Button GameOverButton;
+    public GameObject Player;
+
+    public bool GameOver = false;
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // call this when player has lost
+    public void OnGameOver()
+    {
+        GameOver = true;
+
+        // show gameOverButton
+        GameOverButton.gameObject.SetActive(true);
+
+        // disable Player
+        Player.SetActive(false);
+
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/SpikeBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        bool isPlayer = collision.gameObject.CompareTag("Player");
+
+        if (isPlayer)
+        {
+            ControllerScript.Instance.OnGameOver();
+        }
+    }
+}
